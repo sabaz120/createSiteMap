34bfb39cf32751e4539e8f021bce078bdbff6cfe
Sabas Vega <sabascarloseduardo@gmail.com>
2018-05-28 10:31:31 -0400

    Add modal of Modal aggregate with instructions on how the module works

--- Modules/Createsitemap/Resources/views/admin/sitemaps/viewSiteMapXML.blade.php
+++ Modules/Createsitemap/Resources/views/admin/sitemaps/viewSiteMapXML.blade.php
@@ -9,13 +9,25 @@
         <li class="active">{{ trans('createsitemap::sitemaps.title.create sitemaps') }}</li>
     </ol>
 @stop
-
+@push('css-stack')
+  <style media="screen">
+    .margin20px{
+      text-indent:20px;
+    }
+    .margin40px{
+      text-indent:40px;
+    }
+  </style>
+@endpush
 @section('content')
     <div class="row">
         <div class="col-xs-12">
             <div class="box box-primary">
-                <div class="box-header">
+                <div class="box-header text-center">
+                    <h2><span class="text-primary">Instructions</span> </h2>
+                      <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Read</button>
                 </div>
+                <hr>
                 <!-- /.box-header -->
                 <div class="box-body">
                   @if($success==1)
@@ -31,6 +43,45 @@
                   @endif
                 </div>
                 <!-- /.box -->
+            </div>
+            <div class="col-lg-12">
+              <!-- Modal -->
+              <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog modal-lg">
+                  <!-- Modal content-->
+                  <div class="modal-content">
+                    <div class="modal-header text-primary">
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                      <h4 class="modal-title text-center">Manual of Instruction</h4>
+                    </div>
+                    <div class="modal-body">
+                      <p class="margin20px">This module will take all the active frontend routes of the project whose method is of type show and load them in the sitemap.xml. That is, routes like http://example.com/en/contact/ or http://example.com/en/support-in-line will be loaded in the sitemap.xml</p>
+                      <p class="margin20px">Although if you are managing content like a blog, you will have routes like /en/{$ category-> slug}/, these routes are not taken into account for the construction of the sitemap.xml, therefore, it is necessary to build them with the parameters corresponding to be taken into account at the time of construction of the sitemap.xml.</p>
+                      <p>Here is an example:</p>
+                      <div style="container" style="text-align: justify;">
+                        <span style="color:green">foreach (Category::all()  as $category) {</span>
+                          <p class="margin20px">$router->group(['prefix' => $category->slug], function (Router $router) use ($category) {</p>
+                          <p class="margin40px">$locale = LaravelLocalization::setLocale() ?: App::getLocale();</p>
+                          <p class="margin40px">$router->get('/', [</p>
+                          <p class="margin40px">'as' => $locale . '.iblog.' . $category->slug,</p>
+                          <p class="margin40px">'uses' => 'PublicController@index',</p>
+                          <p class="margin40px">]);</p>
+                          <p class="margin20px">});</p>
+                          <p><span style="color:green">}//foreach</span></p>
+                      </div>
+                      <p class="margin20px">With this foreach which is marked in green, several routes are being created depending on the number of categories that exist in the database, that is, if there are 3 categories (clothes, computing, phones) 3 routes will be created:</p>
+                      <p>1.http://example.com/en/clothes/</p>
+                      <p>2.http://example.com/en/computing/</p>
+                      <p>3.http://example.com/en/phones/</p>
+                      <p class="margin20px">After finishing generating the sitemap.xml, I would recommend commenting the foreach lines, since they are no longer necessary in the project and could cause problems with similar routes</p>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Modal -->
             </div>
         </div>
     </div>
